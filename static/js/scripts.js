c3fde2dafbdc50ca283170f7974f71472c0a5c79
Young Kwak <80361834+young-kwak@users.noreply.github.com>
2021-06-01 00:48:54 -0400

    Remove all files

--- static/js/scripts.js
+++
@@ -1,9 +0,0 @@
-// 게시판
-// 글 삭제
-$(document).ready(function(){
-	$(".delArticle").on('click', function() { 
-			if(confirm("정말로 삭제하시겠습니까?")) {
-				location.href = $(this).data('uri'); 
-			}
-	});
-});
